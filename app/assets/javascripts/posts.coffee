540438516bdd6cbd3b518ffe312c4469a471b37d
Jake Hill <Jake_hill666@hotmail.co.uk>
2017-04-27 15:50:32 +0200

    desperately trying to figure out how to sort posts dynamically

--- app/assets/javascripts/posts.coffee
+++ app/assets/javascripts/posts.coffee
@@ -3,7 +3,6 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on "turbolinks:load", ->
-	console.log("JS Working")
 	$(".post-options .glyphicon").on "click", ->
 	 	if($(this).hasClass("glyphicon-camera"))
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,11 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    if params[:sort].present?
+      @posts = Post.all.reverse
+    else
+      @posts = Post.all
+    end
   end
 
   # GET /posts/1
